026b320d2be3db7d52bc1d5ad0d60561d95dc8f1
Kclient <go4521304@gmail.com>
2022-04-01 15:52:49 +0900

    GameView 생성

---
+++ Project/SampleGame/app/src/main/java/kr/ac/tukorea/sgp2022/samplegame/MainActivity.java
@@ -0,0 +1,16 @@
+package kr.ac.tukorea.sgp2022.samplegame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
