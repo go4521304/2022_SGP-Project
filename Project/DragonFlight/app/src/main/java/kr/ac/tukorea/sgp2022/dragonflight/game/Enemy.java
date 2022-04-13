c08569da4552d3caa171c6af45d7c08e2ea6754f
Kclient <go4521304@gmail.com>
2022-04-13 12:03:15 +0900

    오브젝트 5개가 한줄로 나오게 출략

--- Project/DragonFlight/app/src/main/java/kr/ac/tukorea/sgp2022/dragonflight/game/Enemy.java
+++ Project/DragonFlight/app/src/main/java/kr/ac/tukorea/sgp2022/dragonflight/game/Enemy.java
@@ -5,10 +5,17 @@ import kr.ac.tukorea.sgp2022.dragonflight.framework.Metrics;
 import kr.ac.tukorea.sgp2022.dragonflight.framework.Sprite;
 
 public class Enemy extends Sprite {
+    private static float size;
     protected float dy;
     public Enemy(float x, float y, float speed) {
-        super(x, y, R.dimen.enemy_radius, R.mipmap.f_01_01);
+//        super(x, 0, R.dimen.enemy_radius, R.mipmap.f_01_01);
+        super(x, -size/2, size, size, R.mipmap.f_01_01);
         dy = speed;
+    }
+
+    public static void setSize(float enemySize)
+    {
+        Enemy.size = enemySize;
     }
 
     @Override
--- Project/DragonFlight/app/src/main/java/kr/ac/tukorea/sgp2022/dragonflight/game/EnemyGenerator.java
+++ Project/DragonFlight/app/src/main/java/kr/ac/tukorea/sgp2022/dragonflight/game/EnemyGenerator.java
@@ -6,33 +6,48 @@ import kr.ac.tukorea.sgp2022.dragonflight.R;
 import kr.ac.tukorea.sgp2022.dragonflight.framework.GameObject;
 import kr.ac.tukorea.sgp2022.dragonflight.framework.Metrics;
 
-public class EnemyGenerator implements GameObject {
-    private static final float INITIAL_SPAWN_INTERVAL = 5.0f;
+public class EnemyGenerator implements GameObject
+{
+    private static final float INITIAL_SPAWN_INTERVAL = 2.0f;
     private final float spawnInterval;
     private final float fallSpeed;
     private float elapsedTime;
 
-    public EnemyGenerator() {
+    public EnemyGenerator()
+    {
         this.spawnInterval = INITIAL_SPAWN_INTERVAL;
         this.fallSpeed = Metrics.size(R.dimen.enemy_initial_speed);
+
+        float enemySize = Metrics.width / 5.0f;
+        Enemy.setSize(enemySize);
     }
 
     @Override
-    public void update() {
+    public void update()
+    {
         float frameTime = MainGame.getInstance().frameTime;
         elapsedTime += frameTime;
-        if (elapsedTime > spawnInterval) {
+        if (elapsedTime > spawnInterval)
+        {
             spawn();
             elapsedTime -= spawnInterval;
         }
     }
 
-    private void spawn() {
-        Enemy enemy = new Enemy(Metrics.width / 2, 0, fallSpeed);
-        MainGame.getInstance().add(enemy);
+    private void spawn()
+    {
+        float tenth = Metrics.width / 10;
+        for (int i = 1; i <= 9; i+=2)
+        {
+            float x = i * tenth;
+            Enemy enemy = new Enemy(x, 0, fallSpeed);
+            MainGame.getInstance().add(enemy);
+        }
+
     }
 
     @Override
-    public void draw(Canvas canvas) {
+    public void draw(Canvas canvas)
+    {
     }
 }
